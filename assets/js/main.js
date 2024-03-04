c4ad645637b423728a5b29211a4295693bb4be7d
simoneevacca <v.simone.vacca@gmail.com>
2024-03-04 12:46:54 +0100

    print in console the components of object

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,39 +8,44 @@
 
 const team = [
     {
-      name: 'Wayne Barnett',
-      role: 'Founder & CEO',
-      image: 'wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
-      name: 'Angela Caroll',
-      role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
     },
     {
-      name: 'Walter Gordon',
-      role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
     },
     {
-      name: 'Angela Lopez',
-      role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
     {
-      name: 'Scott Estrada',
-      role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
     },
     {
-      name: 'Barbara Ramos',
-      role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
     },
-  ];
+];
 
-  // stampare su console le informazioni di nome, ruolo e la stringa della foto
-  // ciclare nella stringa e selezionare gli elementi degli oggetti
-  // riportarli in console con un console.log
-  // stampare le stesse informazioni su DOM sottoforma di stringhe
-//   creare i nodi della DOM nei quli inserire i componenti degli ogetti
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+// ciclare nella stringa e selezionare gli elementi degli oggetti
+for (let i = 0; i < team.length; i++) {
+    const member = team[i];
+    console.log(member.name, member.role, member.image);
+    
+}
+// riportarli in console con un console.log
+// stampare le stesse informazioni su DOM sottoforma di stringhe
+// creare i nodi della DOM nei quli inserire i componenti degli ogetti
