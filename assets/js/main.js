eb99d81259e01cdf29c1e401b27a11684f803875
simoneevacca <v.simone.vacca@gmail.com>
2024-03-04 12:43:38 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// Viene fornito un file js in cui è definito un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  // stampare su console le informazioni di nome, ruolo e la stringa della foto
+  // ciclare nella stringa e selezionare gli elementi degli oggetti
+  // riportarli in console con un console.log
+  // stampare le stesse informazioni su DOM sottoforma di stringhe
+//   creare i nodi della DOM nei quli inserire i componenti degli ogetti
